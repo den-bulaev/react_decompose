905f96b5f4e14e41ca8e0189d26c2b331035ea86
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-18 11:38:17 +0300

    Updates

---
+++ precache-manifest.9536fc3aadfe38ab9a0e6bcc7d04ea17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8251eedf6c100e02e3d6e506c9e1bf0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b980bb2902ee572c7106",
+    "url": "./static/css/main.b76adb00.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b980bb2902ee572c7106",
+    "url": "./static/js/main.cc960674.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
